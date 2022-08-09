91fd267e723be695ceacc1f6e33ad8b34b701a88
Ankit Yadav <ankityadav211200@gmail.com>
2022-08-09 14:02:18 +0530

    update us

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,4 +35,82 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+// problem 01
+
+router.get('/movies', function (req, res){
+    let movies = ['MS DHONI', 'PUSHPA', '1983','DON']
+    res.send(movies)
+})
+// problem 02 & 03
+router.get('/movies/:IndexNumber', function (req, res){
+    let movies = ["MS DHONI", "PUSHPA", "1983"]
+    let requestParams = req.params
+    if(requestParams.IndexNumber<movies.length){
+        res.send(movies[requestParams.IndexNumber])
+    }else{
+        res.send('Enter a valid IndexNumber')
+
+    } 
+})
+
+// problem 04
+
+router.get("/Films", function (req, res){
+    let Films = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       
+    res.send(Films)
+})
+
+//problem 05
+
+router.get("/films/:filmId", function(req, res){
+    const films = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+       let filmId = req.params.filmId
+
+      
+       for(let i = 0; i < films.length; i++){
+           let film = films[i]
+           if(film.id == filmId) {
+               
+               return res.send(film)
+           }
+       }
+
+       
+       res.send("The film id doesn't exit")
+})
+
+
+
+
+
+
+
+
+
 module.exports = router;
