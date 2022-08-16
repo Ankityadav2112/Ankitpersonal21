f5b616defc2b18f50bf58ade4e6a53171a781ecb
Ankit Yadav <ankityadav211200@gmail.com>
2022-08-16 22:11:47 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const app = express.Router();
 
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
@@ -7,67 +8,24 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// problem 1
 
+app.get("/sol1", function (req, res) {
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let nth= arr.pop()
+    let consecutiveSum= nth * (nth+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+ 
 
 module.exports = router;
+module.exports = app;
