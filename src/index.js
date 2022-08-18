c4f2bf4679428ef9cdcfff39bf08eee9c2208b5c
Ankit Yadav <ankityadav211200@gmail.com>
2022-08-18 09:49:43 +0530

    updstr

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Ankit2112:CNACVa3h9HsywbEp@cluster0.nwokqr6.mongodb.net/ankitbook1?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
