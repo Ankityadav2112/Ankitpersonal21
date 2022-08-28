269600018d157c21f4ea346b26365d4a50fbf4bb
Ankit Yadav <ankityadav211200@gmail.com>
2022-08-28 10:40:49 +0530

    up

--- src/routes/route.js
+++ src/routes/route.js
@@ -25,6 +25,87 @@ app.get("/sol1", function (req, res) {
   
     res.send(  { data: missingNumber  }  );
   });
+
+  // sol2
+
+  app.get("/sol2", function (req, res) {
+
+    let arr= [33,34,35,37,38]
+    let len =arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+    
+    let firstnum =arr[0]
+    let nth= arr.pop()
+    let consecutiveSum= (len +1)* (firstnum+nth)/2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+
+
+  //  post request - player and booking
+
+  let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+app.post('/players', function (req, res) {
+    
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+
+    //let player = players.find(p => p.name == newPlayersName)
+
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+
+    //undefined is same as false/ a falsy value
+    if (isNameRepeated) {
+        //Player exists
+        res.send("This player was already added!")
+    } else {
+        //New entry
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
+
  
 
 module.exports = router;
